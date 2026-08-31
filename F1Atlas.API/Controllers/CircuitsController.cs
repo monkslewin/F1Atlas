4ef1742822d243d11feb35a229b446007f3aca07
Finn Monks-Lewin <finnml04@icloud.com>
2026-08-31 15:41:58 +1000

    Exposed endpoint to frontend

--- F1Atlas.API/Controllers/CircuitsController.cs
+++ F1Atlas.API/Controllers/CircuitsController.cs
@@ -11,7 +11,7 @@ public class CircuitsController : ControllerBase
     {
         _F1Service = F1Service;
     }
-    
+
     // expose service so frontend can access circuit data
     [HttpGet]
     public async Task<IActionResult> GetCircuits()
@@ -19,6 +19,14 @@ public class CircuitsController : ControllerBase
         var circuits = await _F1Service.GetCircuits();
 
         return Ok(circuits);
+    }
+
+    [HttpGet("{circuitId}/statistics")]
+    public async Task<IActionResult> GetStatistics(string circuitId)
+    {
+        var statistics = await _F1Service.GetCircuitStatistics(circuitId);
+
+        return Ok(statistics);
     }
 
 }
